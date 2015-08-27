d7e24f8dcf06f7d990cb176e22cb8ecf1c98bd6e
gaolili <1076787210@qq.com>
2015-08-27 18:48:04 +0800

    -------

---
+++ ScrollerViewDemoTests/ScrollerViewDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  ScrollerViewDemoTests.m
+//  ScrollerViewDemoTests
+//
+//  Created by gaolili on 15/8/14.
+//  Copyright (c) 2015年 mRocker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ScrollerViewDemoTests : XCTestCase
+
+@end
+
+@implementation ScrollerViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
